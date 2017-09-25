c8b6e6d3732964d5616c45daf82c1f6ed9811d14
Buck Ryan <buck.ryan@gmail.com>
2017-09-25 17:58:44 -0400

    Use latest singer-python, add inclusion property
    Like https://github.com/singer-io/tap-zendesk-chat/pull/3

--- tap_xero/__init__.py
+++ tap_xero/__init__.py
@@ -2,7 +2,7 @@
 import os
 import singer
 from singer import metrics, utils
-from singer.catalog import Catalog
+from singer.catalog import Catalog, CatalogEntry, Schema
 from .pull import (
     IncrementingPull,
     BankTransfersPull,
@@ -104,17 +104,18 @@ def load_schema(tap_stream_id):
 
 
 def discover():
-    result = {"streams": []}
+    catalog = Catalog([])
     for stream in STREAMS:
-        schema = load_schema(stream.tap_stream_id)
-        schema["selected"] = False
-        result["streams"].append(
-            dict(stream=stream.tap_stream_id,
-                 tap_stream_id=stream.tap_stream_id,
-                 key_properties=stream.pk_fields,
-                 schema=schema)
-        )
-    return Catalog.from_dict(result)
+        schema = Schema.from_dict(load_schema(stream.tap_stream_id),
+                                  inclusion="automatic")
+        schema.selected = False
+        catalog.streams.append(CatalogEntry(
+            stream=stream.tap_stream_id,
+            tap_stream_id=stream.tap_stream_id,
+            key_properties=stream.pk_fields,
+            schema=schema,
+        ))
+    return catalog
 
 
 def run_stream(config, state, stream):
