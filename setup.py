1ed2b12885b12e01bfa85b66e6424e69536be4d8
Collin Simon <csimon@talend.com>
2020-10-14 14:10:03 +0000

    Bump to v2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-xero",
-      version="1.0.4",
+      version="2.0.0",
       description="Singer.io tap for extracting data from the Xero API",
       author="Stitch",
       url="http://singer.io",
