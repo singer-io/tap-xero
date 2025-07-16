e568087256808943ff948721c926099003291580
Ben Allred <ben.allred@qlik.com>
2025-07-16 12:01:38 -0600

    Bump tap version and update changelog (#123)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="tap-xero",
-      version="2.3.1",
+      version="2.3.2",
       description="Singer.io tap for extracting data from the Xero API",
       author="Stitch",
       url="http://singer.io",
