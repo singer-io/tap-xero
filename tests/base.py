1c1a6814bfc1ff63749267ac9ae8fc4205fa77cc
Prijen Khokhani <88327452+prijendev@users.noreply.github.com>
2026-03-27 17:34:08 +0530

    Enable dev mode

--- tests/base.py
+++ tests/base.py
@@ -34,7 +34,7 @@ class XeroScenarioBase(unittest.TestCase):
             missing_envs = missing_creds + missing_props
             raise Exception("set " + ", ".join(missing_envs))
         self._credentials = {k: os.getenv(v) for k, v in required_creds.items()}
-        self.conn_id = connections.ensure_connection(self)
+        self.conn_id = connections.ensure_connection(self, payload_hook=preserve_refresh_token)
 
     def get_type(self):
         return "platform.xero"
